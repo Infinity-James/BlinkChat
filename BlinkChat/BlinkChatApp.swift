65b26bd5df59d939f8a77e7a8ca627ed00e046e6
James Valaitis <james@infinityjames.com>
2024-02-13 12:23:05 +0000

    Initial Commit

---
+++ BlinkChat/BlinkChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlinkChatApp.swift
+//  BlinkChat
+//
+//  Created by James Valaitis on 13/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct BlinkChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlinkChat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BlinkChat
+//
+//  Created by James Valaitis on 13/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
